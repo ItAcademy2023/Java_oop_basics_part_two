ea752cb79ba4f52d87e2bffabbfdeb2484abd049
howard <howardtchienkoua@yahoo.com>
2023-03-09 16:06:07 +0200

    re

---
+++ src/com/itacademy/eshop/product/types/Food.java
@@ -0,0 +1,44 @@
+package com.itacademy.eshop.product.types;
+
+import com.itacademy.eshop.product.Product;
+
+public class Food extends Product {
+    private final String FoodName;
+    private final String FoodSize;
+    private final int weight;
+
+    public Food (String name, double price, Category category, String FoodName, String FoodSize, int weight){
+        super(name, price, category);
+        this.FoodName = FoodName;
+        this.FoodSize = FoodSize;
+        this.weight = weight;
+
+    }
+
+    public String getFoodName() {
+        return FoodName;
+    }
+
+    public String getFoodSize() {
+        return FoodSize;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public void displayProductInfo() {
+        // display the list of the products via Products(parent class) till CLothing(child class) of the implementation
+
+        //getter methods of the parent class
+        System.out.println(getName());
+        System.out.println(getPrice());
+        System.out.println(getCategory());
+        //getter methods of the child class
+        System.out.println(this.FoodName);
+        System.out.println(this.FoodSize);
+        System.out.println(this.weight);
+
+    }
+}
