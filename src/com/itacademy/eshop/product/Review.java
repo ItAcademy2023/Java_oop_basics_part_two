eb6373bfbcd0f889b5f2f31157917984718113fa
linas159 <linas.mintautiskis@gmail.com>
2023-03-09 15:57:56 +0200

    Encapsulation

--- src/com/itacademy/eshop/product/Review.java
+++ src/com/itacademy/eshop/product/Review.java
@@ -1,13 +1,25 @@
 package com.itacademy.eshop.product;
 
 public class Review {
-    public String author;
-    public int rating;
-    public String comment;
+    private String author;
+    private int rating;
+    private String comment;
 
     public Review(String author, int rating, String comment) {
         this.author = author;
         this.rating = rating;
+        this.comment = comment;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public void setComment(String comment) {
         this.comment = comment;
     }
 
